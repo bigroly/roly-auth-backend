5a766e64c9a2f2a9115e6541b82d7017f7e0ab74
Rolando Chancellor <chancellor.rolando@gmail.com>
2024-06-30 12:51:29 +1000

    .net 8 and dependency updates

--- BuildAndDeploy.cmd
+++ BuildAndDeploy.cmd
@@ -4,10 +4,10 @@ echo DotNet restore
 dotnet restore
 
 echo DotNet install AWS Tools
-dotnet tool install -g Amazon.Lambda.Tools --framework net6.0
+dotnet tool install -g Amazon.Lambda.Tools --framework net8
 
 echo DotNet Build
-dotnet lambda package --configuration Release --framework net6.0 --output-package ../../dist/backendFunction.zip
+dotnet lambda package --configuration Release --framework net8 --output-package ../../dist/backendFunction.zip
 
 echo Deploying via CDK
 cd ../../../
